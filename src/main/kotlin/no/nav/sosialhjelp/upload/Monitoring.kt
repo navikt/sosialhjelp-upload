d40a603127870717b9a32cc2298d0580cfb36a16
Martin Almvik <martin.almvik@nav.no>
2025-09-17 15:49:07 +0200

    ikke logg ut kall til /internal

--- src/main/kotlin/no/nav/sosialhjelp/upload/Monitoring.kt
+++ src/main/kotlin/no/nav/sosialhjelp/upload/Monitoring.kt
@@ -19,7 +19,7 @@ fun Application.configureMonitoring() {
     }
     install(CallLogging) {
         level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
+        filter { call -> !call.request.path().startsWith("/internal") }
     }
     routing {
         get("/metrics-micrometer") {
