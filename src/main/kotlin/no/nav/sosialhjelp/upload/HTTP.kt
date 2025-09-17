5efc19737811cc330055bf2d1006e00ce009e322
Martin Almvik <martin.almvik@nav.no>
2025-09-17 15:26:44 +0200

    Fjern ktor-server-openapi

--- src/main/kotlin/no/nav/sosialhjelp/upload/HTTP.kt
+++ src/main/kotlin/no/nav/sosialhjelp/upload/HTTP.kt
@@ -5,8 +5,6 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.defaultheaders.*
 import io.ktor.server.plugins.forwardedheaders.*
-import io.ktor.server.plugins.openapi.*
-import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
 fun Application.configureHTTP() {
@@ -25,8 +23,5 @@ fun Application.configureHTTP() {
     }
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
-    }
-    routing {
-        openAPI(path = "openapi")
     }
 }
