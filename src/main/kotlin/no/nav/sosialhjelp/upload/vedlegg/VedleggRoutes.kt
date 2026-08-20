99e6a926ac96b0695098459604d17e061ad05e91
Martin Almvik <martin.almvik@nav.no>
2026-08-20 14:38:21 +0200

    Endepunkt for sletting av alle dokumenter på soknadId/kategori

--- src/main/kotlin/no/nav/sosialhjelp/upload/vedlegg/VedleggRoutes.kt
+++ src/main/kotlin/no/nav/sosialhjelp/upload/vedlegg/VedleggRoutes.kt
@@ -1,10 +1,13 @@
 package no.nav.sosialhjelp.upload.vedlegg
 
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
 import io.ktor.server.auth.authenticate
 import io.ktor.server.plugins.di.dependencies
 import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.application
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import no.nav.sosialhjelp.upload.verifyNavEksternRefIdOwnershipByPid
@@ -13,16 +16,31 @@ fun Route.configureVedleggRoutes() {
     val vedleggService: VedleggService by application.dependencies
 
     route("/vedlegg") {
-        // GET /vedlegg/{navEksternRefId} — return JsonVedleggSpesifikasjon for the given soknadId.
         // Called machine-to-machine by sosialhjelp-soknad-api; uses TokenX auth.
         authenticate("tokenx") {
             route("/{navEksternRefId}") {
                 verifyNavEksternRefIdOwnershipByPid()
 
+                // GET /vedlegg/{navEksternRefId} — return JsonVedleggSpesifikasjon for the given soknadId.
                 get {
                     val navEksternRefId = call.parameters["navEksternRefId"]!!
                     val vedlegg = vedleggService.getVedleggByNavEksternRefId(navEksternRefId)
                     call.respond(vedlegg)
+                }
+
+                delete("/{kategori}") {
+                    val navEksternRefId =
+                        call.parameters["navEksternRefId"] ?: return@delete call.respondText(
+                            "Missing navEksternRefId",
+                            status = BadRequest,
+                        )
+                    val kategori =
+                        call.parameters["kategori"] ?: return@delete call.respondText(
+                            "Missing kategori (vedleggstype)",
+                            status = BadRequest,
+                        )
+
+                    vedleggService.deleteVedlegg(navEksternRefId, kategori)
                 }
             }
         }
